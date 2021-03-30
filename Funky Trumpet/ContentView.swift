c8fc78fe963f0d4dd2fb8a2e0d8fe9b6e436e468
Paul Circo <paul.circo@gmail.com>
2021-03-30 12:52:44 -0500

    initial commit

---
+++ Funky Trumpet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Funky Trumpet
+//
+//  Created by Paul Circo on 3/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Funky Trumpet/Funky_TrumpetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Funky_TrumpetApp.swift
+//  Funky Trumpet
+//
+//  Created by Paul Circo on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Funky_TrumpetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
